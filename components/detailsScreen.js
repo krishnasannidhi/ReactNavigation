db03e7de9833a7e4cae4252c8d883831959dfe71
k0s02oi <krishnamurthy.sannidhi@walmart.com>
2020-02-22 13:27:41 -0600

    enabled navigation between screens

--- components/detailsScreen.js
+++ components/detailsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-export default function DetailsScreen(){
+export default function DetailsScreen({navigation}){
     return (
         <View style={{flex:1, alignItems:'center', justifyContent: 'center'}}>
             <Text>Details Screen</Text>
+            <Button title="Go to Details... again" onPress={()=>navigation.push('Details')}></Button>
         </View>
     )
 }
